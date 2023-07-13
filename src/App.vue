ad9ecf2df6741553b372a0c5654cd85faf7707bc
zanniadevweb <zanni.a.dev.web@gmail.com>
2023-07-13 20:37:20 +0200

    add missing files

---
+++ src/App.vue
@@ -0,0 +1,93 @@
+<script setup>
+    import HelloWorld from './components/HelloWorld.vue'
+    import TheWelcome from './components/TheWelcome.vue'
+</script>
+<script>
+    import { reactive, computed, defineComponent } from 'vue'
+
+    const author = reactive({
+        name: 'John Doe',
+        books: [
+            'Vue 2 - Advanced Guide',
+            'Vue 3 - Basic Guide',
+            'Vue 4 - The Mystery'
+        ]
+    })
+
+    // Computed method
+    const publishedBooksMessage = computed(() => {
+        return author.books.length > 0 ? 'Yes' : 'No'
+    })
+
+    export default defineComponent({
+        data() {
+            return {
+                firstname: 'Dave',
+                lastname: 'Webb',
+                languages: ['HTML', 'CSS', 'PHP', 'JS', 'Python']
+            }
+        },
+        computed: {
+            doubleCount() {
+                return this.count * 2
+            }
+        },
+        methods: {
+            increment() {
+                this.count++
+            }
+        },
+        watch: {
+            count(newValue, oldValue) {
+                // do something when count changes
+            }
+        },
+        mounted() {
+            // do something when the component is mounted
+        }
+    })
+</script>
+
+<template>
+    <header>
+        <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+        <span>{{ publishedBooksMessage }}</span>
+        <p>{{ firstname }} {{ lastname}} {{ languages.length >= 5 ? "est confirmé" : "est encore en apprentissage" }}.</p>
+        <div class="wrapper">
+            <HelloWorld msg="You did it!" />
+        </div>
+    </header>
+
+    <main>
+        <TheWelcome />
+    </main>
+</template>
+
+<style scoped>
+    header {
+        line-height: 1.5;
+    }
+
+    .logo {
+        display: block;
+        margin: 0 auto 2rem;
+    }
+
+    @media (min-width: 1024px) {
+        header {
+            display: flex;
+            place-items: center;
+            padding-right: calc(var(--section-gap) / 2);
+        }
+
+        .logo {
+            margin: 0 2rem 0 0;
+        }
+
+        header .wrapper {
+            display: flex;
+            place-items: flex-start;
+            flex-wrap: wrap;
+        }
+    }
+</style>
